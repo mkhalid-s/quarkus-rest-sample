694515ccf9f17bc80cac825a301f6bab46ce8116
Khalid Shaikh <khalid.spy@gmail.com>
2021-07-21 22:04:30 +0530

    update pom

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -4,13 +4,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Path("/hello")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String hello() throws UnknownHostException {
+        return "Hello RESTEasy from host: "
+                + InetAddress.getLocalHost().getHostName()
+                + " and IP: "+InetAddress.getLocalHost().getHostAddress();
     }
 }
